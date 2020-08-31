bb5765885f346f508c8a590e4e749efb3cf9bd4e
oddhus <huseby.oddmund@gmail.com>
2020-08-31 20:30:09 +0200

    init commit

---
+++ src/main/java/com/huseby/jpa/simple/model/Todo.java
@@ -0,0 +1,21 @@
+package com.huseby.jpa.simple.model;
+
+import lombok.Data;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String summary;
+    private String description;
+
+}
